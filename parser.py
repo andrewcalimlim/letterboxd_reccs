c5bea98adccce82adafb1d94521289d97bcf3846
Andrew Calimlim <Andr3wCalimlim@gmail.com>
2021-07-22 21:52:28 -0700

    added matrix conversion to parser.py
    converting these dictionaries to numpy matrices, whew!

--- parser.py
+++ parser.py
@@ -1,7 +1,12 @@
-#import numpy as np
 import csv
+import os
+import numpy as np
 
-def csv_to_dict(file_name):
+###
+# Parses csv file into an actual dictionary of ratings with unique film title (year) keys
+# returns a dictionary
+###
+def getRatingsDict(file_name):
     ratings = {}
 
     file_path = 'dataset/ratings/' + file_name
@@ -34,5 +39,66 @@ def csv_to_dict(file_name):
     
     return ratings
 
-test = csv_to_dict('mal tose.csv')
-print(test)
+###
+# Given a file path of csv files and specifying which csv file is "you"
+# turns the csv files into an alphabetized matrix of ratings
+# each user is a row, and each column is a film
+# ratings are from 1-10, 0 indicates unseen (unrated viewings don't appear in ratings)
+# how are we supposed to know how you feel about a movie without a rating???
+#
+# returns two matrices, one with all other users' ratings
+# the other is "your" ratings
+###
+
+def getMatrices(fp, myfile):
+    all_films = set()
+    others_index = {}
+    others_ratings = []
+    my_ratings = {}
+
+    i = 0
+    directory = fp
+    for filename in os.listdir(directory): #iterating per file
+
+        ratings = getRatingsDict(filename) #parsing ratings csv file into ratings dict
+
+        for film in ratings.keys(): #all movies represented in all_films
+            all_films.add(film)
+
+        if filename == myfile: #but only ratings get represented in the other data structs
+            my_ratings = ratings
+            continue
+
+        others_index[i] = filename #this is just for debug, i may remove it for clarity later
+        others_ratings.append(ratings)
+
+        i = i + 1
+
+    all_films = sorted(all_films)
+
+    #print(all_films)
+
+    #creating matrices
+    others_matrix = np.zeros((len(others_ratings), len(all_films)))
+    my_matrix = np.zeros((1, len(all_films)))
+
+    for i in range(len(others_ratings)):
+        for j in range(len(all_films)):
+            if all_films[j] in others_ratings[i]:
+                others_matrix[i,j] = others_ratings[i][all_films[j]]
+
+    for i in range(len(all_films)):
+        if all_films[i] in my_ratings:
+            my_matrix[0,i] = my_ratings[all_films[i]]
+
+    #print(others_index)
+
+    return my_matrix, others_matrix
+
+"""
+test_fp = r'/Users/andrew/Documents/PROJ/dataset/ratings/'
+test_myfile = 'me.csv'
+
+mine, theirs = getMatrices(test_fp, test_myfile)
+print(theirs)
+"""
