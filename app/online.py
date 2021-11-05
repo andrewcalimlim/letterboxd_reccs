7170b1e4f9aeca7e4a8f113d938743b614f0186c
Andrew Calimlim <Andr3wCalimlim@gmail.com>
2021-11-05 16:44:05 -0700

    archiving filter
    too hard, idk how to pass variable long strings from an HTML Flask checkbox, kinda hard tbh

--- app/online.py
+++ app/online.py
@@ -10,22 +10,28 @@ def input():
 
 @app.route('/output',methods = ['POST', 'GET'])
 def output():
-   if request.method == 'POST':
+    if request.method == 'POST':
         you = request.form['you']
         usernames = request.form['usernames']
-        convergence = float(request.form['converge'])
+        #convergence = float(request.form['converge'])
 
         usernames = usernames.split()
         usernames.append(you)
 
         raw_scores, who, films = getMatrices(usernames)
         print("approximating..")
-        scores = rank1Approximate(raw_scores, convergence)
+        scores = rank1Approximate(raw_scores, 0.01)
 
-        reccs = recommendation(raw_scores, scores, who, films, you)
+        reccs = recommendation(raw_scores, scores, who, films, [], you)
 
-        return render_template('output.html', name = you, film1=reccs[0], 
-        film2=reccs[1], film3=reccs[2], film4=reccs[3], film5=reccs[4])
+        return render_template('output.html', name = you, film1=reccs[0], film2=reccs[1], film3=reccs[2], film4=reccs[3], film5=reccs[4], you=you, others=usernames)
+
+@app.route('/test',methods = ['POST', 'GET'])
+def test():
+    if request.method == 'POST':    
+        skips = request.form.getlist('seen')
+        print(skips[0])
+        return render_template('test.html', seen_it = skips)
 
 if __name__ == '__main__':
     app.run(debug=True)
